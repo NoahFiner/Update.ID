074a7161d02f27b4ac65aae8d582609c8d50c651
nfiner <nfiner@umich.edu>
2019-03-01 04:16:06 -0500

    Update test files and downgrade to v1.0

--- updater.py
+++ updater.py
@@ -142,7 +142,7 @@ Would you like to update? (y/n):""".format(
 
 
 def special_function():
-    print("Hello from version 2.5!")
+    print("Hello from version 1.0!")
 
 if __name__ == "__main__":
     updater = Updater()
