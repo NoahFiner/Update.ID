7037b8596f8c78d96dcf6534132709c41b1a9d51
nfiner <nfiner@umich.edu>
2019-03-01 01:31:49 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Update.ID
+Update.ID is a program that checks if it's currently updated or not and, if it needs updating, updates itself, restarts itself, and runs again. Wow!
+
+Instead of opting to build a webserver that hosts some files, Update.ID currently uses the extremely high-tech solution of checking another GitHub repository for potential updates. Wow! Honestly, git is the new Go.
+
+## Features
+To utilize modern security concepts, Update.ID ensures no attackers can provide malicious updates by generating a unique MD5 hash of updater.py in config.json on the server-side. Unless an attacker modifies config.json, updater.py will only accept updates that match the MD5 hash provided by the remote config.json.
+
+## Installation
+Clone this repo
+Boot up the virtual env
+```bash
+source .env/bin/activate
+```
+Run updater.py
+```bash
+python3 updater.py
+``` 
+
+## Pushing an update
+To use my revolutionary alternative to a server, aka GitHub, see https://github.com/NoahFiner/groove-remote.
+
+On a serious note, if you'd like to I would be more than happy to write some actual back-end code for serving up files. I categorized an architecture of just a few GET requests to be more on the trivial.
+
+## Assumptions
+There were a few assumptions I had to make
+1) Our program is extremely simple and consists of only one file without any modules.
+2) Attackers do not have access to config.json on our GitHub "webserver".
+3) GitHub is a valid replacement for a web server.
+4) A sysadmin has the patience to use a hacky method for pushing updates.
+
+## Solutions
+1) Put all of the files (including in directories and subdirectories) of our program in a queue and update all of them one-by-one by popping. End by rewriting to updater.py itself then restart it. This would involve much more complicated back-end and therefore a non-GitHub server.
+2) Make sure hackers can't access GitHub accounts, preferably see #3.
+3) Create a simple server that has a few endpoints returning config.json as actual json, the text of updater.py, and maybe more files. Include documentation for a sysadmin to upload updated files and update a config.json or specified endpoints.
+4) See #3.
+
+## More improvements
+1) Use YAML instead of JSON if the back-end is written in Flask. If it's written in JS or Go, we should stick with JSON.
---
+++ updater.py
@@ -0,0 +1,108 @@
+from importlib import reload
+import json
+import requests
+import hashlib
+
+class Updater:
+
+    # Get the most recent commit, the local config, and the remote config.
+    # Save those to Updater.
+    def __init__(self):
+        # We need to start by getting the most recent commit sha
+        # since just accessing the file through githubusercontent.com
+        # will cache it at a specific URL permanently.
+        r = requests.get("https://api.github.com/repos/NoahFiner/groove-remote/git/refs/heads/master")
+        response = json.loads(r.text)
+        self.commit = response["object"]["sha"]
+
+        # Read from our local config
+        with open('config.json', 'r') as f:
+            self.local_config = json.load(f)
+
+        # Read from the remote config
+        r = requests.get("https://raw.githubusercontent.com/NoahFiner/groove-remote/{0}/config.json".format(self.commit))
+        self.remote_config = json.loads(r.text)
+
+    # Checks if there is there a difference between the remote and local
+    # config. If there is, then ask the user if they want to update or not.
+    #
+    # Returns true if we should run the current version of the program
+    # and false if we should update it. 
+    def should_run_current_version(self):
+        if self.local_config["version"] != self.remote_config["version"]:
+            response = input("""
+
+There is currently an update available (version {version}).
+Notes about this new version: {notes}
+
+You are currently at version {version_old}.
+Would you like to update? (y/n):""".format(
+                        version=self.remote_config["version"],
+                        notes=self.remote_config["notes"],
+                        version_old=self.local_config["version"]))
+                
+            return response != "y"
+        else:
+            print("Up to date!")
+            return True
+
+    # Updates the module 
+    def update(self):
+        # Fetch the updated groove_program.py from GitHub
+        print("Fetching version "+self.remote_config["version"]+"...", end='')
+        r = requests.get("https://raw.githubusercontent.com/NoahFiner/groove-remote/{0}/updater.py".format(self.commit))
+        print("done!")
+
+        # Write that to the current groove_program.py and reload it
+        print("Updating to "+self.remote_config["version"]+"...", end='')
+        file = open(__file__, "w")
+        file.write(r.text)
+        file.close()
+
+        # Update the current config.json
+        file = open("config.json", "w")
+        file.write(json.dumps(self.remote_config))
+        file.close()
+        print("done!")
+
+        print("Checking MD5 hash with config...", end='')
+        if(self.get_md5(__file__)
+                                        == self.remote_config["hash"]):
+            print("verified!")
+
+            print("Restarting the updater...\n\n")
+            # Restart the program
+            import os
+            import sys
+            os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)
+        else:
+            print("UNVERIFIED!")
+            print("Please use generate_update.py to generate a valid hash")
+            print("Exiting...")
+
+    # Helper function for getting an MD5 hash of a file and determining
+    # authenticity of an update
+    def get_md5(self, filename):
+        md5 = hashlib.md5()
+        file = open(filename, "r")
+        while True:
+            data = file.read(32)
+            if not data:
+                break
+            md5.update(data.encode("utf-8"))
+        return md5.hexdigest()
+
+
+def special_function():
+    print("Hello from version 2.0!")
+
+if __name__ == "__main__":
+    updater = Updater()
+
+    if(updater.should_run_current_version()):
+        print("Running with version {0}".format(
+                                            updater.local_config["version"]))
+        
+        special_function()
+    else:
+        updater.update()
